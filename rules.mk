50995a7fcdcdd7cc1ec43d61d14522ccdd94fd43
ddrcode <ddrcode@gmail.com>
2022-10-21 16:08:47 +0100

    added debugging messages

--- rules.mk
+++ rules.mk
@@ -8,17 +8,28 @@ KEY_OVERRIDE_ENABLE = yes
 # BOOTMAGIC_ENABLE = no
 # TERMINAL_ENABLE = yes
 
-ENCODER_ENABLE = no
-ENCODER_MAP_ENABLE = no
+$(info [ddrcode/rules.mk] start for keyboard: $(strip $(KEYBOARD)))
 
-# settings not for 32kb RAM keyboards
-ifneq ($(filter $(strip $(KEYBOARD)),"converter/usb_usb/hasu" "idobao/id75/v1"),)
-WPM_ENABLE = yes
-EXTRAKEY_ENABLE = yes
-CONSOLE_ENABLE = yes
+ifneq ($(filter $(strip $(KEYBOARD)),converter/usb_usb/hasu idobao/id75/v1),)
+    # settings for boards with 32kB of RAM
+    $(info [ddrcode/rules.mk] applying ram<=32kB settings)
+    MOUSEKEY_ENABLE = no
+    POINTING_DEVICE_ENABLE = no
 else
-MOUSEKEY_ENABLE = no
-POINTING_DEVICE_ENABLE = no
+    # settings for boards with more RAM
+    $(info [ddrcode/rules.mk] applying ram>32kB settings)
+    WPM_ENABLE = yes
+    EXTRAKEY_ENABLE = yes
+    CONSOLE_ENABLE = yes
+endif
+
+ifeq ($(strip $(KEYBOARD)),keychron/q10/q10_ansi_stm32l432_ec11)
+    # settings for boards with encoder
+    $(info [ddrcode/rules.mk] applying encoder settings)
+    ENCODER_ENABLE = yes
+    ENCODER_MAP_ENABLE = no
 endif
 
 SRC += ddrcode.c
+
+$(info [ddrcode/rules.mk] end)
