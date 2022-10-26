6ec2b1bd02e74fa58e61b3ae3fbc9970f0846d5e
ddrcode <ddrcode@gmail.com>
2022-10-26 19:17:05 +0100

    Improved OSM toggling (addition to #5) with docs

--- features/osm.c
+++ features/osm.c
@@ -24,45 +24,53 @@ static uint8_t osm_tap_flags = 0;
 static const uint8_t tapflags_mask = 0b10001111;
 
 
+// keycode encodes mods on 5 bits. Convertion to osm_tap_flags
 static uint8_t keycode2tapflags(uint16_t keycode) {
-    const uint8_t mod = keycode & ~QK_ONE_SHOT_MOD;
-    if (mod & 0b00010000) { // r-modifiers
-        if (mod & 0b10000000) return 0b10000000; // AltGr
-        return mod & 0b00001111;
-    }
-    return mod;
+    const uint8_t mod = keycode & 0x1f; // modifier flags only (last 5 bits)
+    if (mod == 0b00011000) return 0b10000000; // AltGr
+    return mod & 0b00001111; // all other modifiers
 }
 
 
-static uint8_t osmmods2tapflags(uint8_t mods) {
-    return (mods & tapflags_mask) | ((mods & ~tapflags_mask) >> 4);
+// OSM callback encodes mods on 8 bits. Convertion to osm_tap_flags
+static uint8_t osmmods2tapflags(uint8_t osmmods) {
+    return (osmmods & tapflags_mask) | ((osmmods & ~tapflags_mask) >> 4);
 }
 
 
+// Required for cases when OSM state changes programatically
 void oneshot_mods_changed_user(uint8_t mods) {
     osm_tap_flags &= osmmods2tapflags(mods);
 }
 
 
+// to be invoked from process_record_user function (see ddrcode.c)
 bool process_record_user_osm(uint16_t keycode, keyrecord_t *record) {
     if (!record->event.pressed) return true;
 
-    if (keycode & QK_ONE_SHOT_MOD) {
-        const uint8_t mod = keycode2tapflags(keycode);
-        if (record->tap.count > 1) {
-            osm_tap_flags &= ~mod;
-            return true;
-        }
-        if (osm_tap_flags & mod) {
-            del_oneshot_mods(mod | ((mod << 4) & ~tapflags_mask));
-            osm_tap_flags &= ~mod;
-            return false;
-        }
-        osm_tap_flags |= mod;
-    } else {
+    // reset state on non-OSM key press
+    if (keycode <= QK_ONE_SHOT_MOD || keycode > QK_ONE_SHOT_MOD_MAX) {
         osm_tap_flags = 0;
+        return true;
     }
-    
+
+    const uint8_t mod = keycode2tapflags(keycode);
+
+    // dont toggle on multi-tap
+    if (record->tap.count > 1) {
+        osm_tap_flags &= ~mod;
+        return true;
+    }
+
+    // toggle if flag is present and stop tap processing
+    if (osm_tap_flags & mod) {
+        del_oneshot_mods(mod | ((mod << 4) & ~tapflags_mask));
+        osm_tap_flags &= ~mod;
+        return false;
+    }
+
+    // first OSM tap - set the flag and continue
+    osm_tap_flags |= mod;   
     return true;
 }
 
